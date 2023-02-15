e89c0bc0d686d438d2976dc4728963edad82e937
mikukula <mikukula123@gmail.com>
2023-02-15 14:36:03 +0800

    pdf generator takes parameters

--- greenwood/connectivity/pdf_generator.py
+++ greenwood/connectivity/pdf_generator.py
@@ -1,25 +1,29 @@
 import jinja2
 import pdfkit
+def generatePdf(elephant_frequency:int, motorbike_frequency:int, gunshot_frequency:int, human_frequency:int, 
+                logging_frequency:int, logging_occurences:int, poaching_occurences:int):
+    elephant_f = elephant_frequency
+    motorbike_f = motorbike_frequency
+    gunshot_f = gunshot_frequency
+    human_f = human_frequency
+    logging_f = logging_frequency
+    logging_o = logging_occurences
+    poaching_o = poaching_occurences
 
-elephant_f = 83
-motorbike_f = 93
-gunshot_f = 50
-human_f = 0
-logging_f = 0
-logging_o = 0
-poaching_o = 0
 
+    context = {'ef': elephant_f, 'mf': motorbike_f, 
+            'gf': gunshot_f, 'hf': human_f, 'lf' : logging_f, 
+            'lo' : logging_o, 'po' : poaching_o}
 
-context = {'ef': elephant_f, 'mf': motorbike_f, 
-           'gf': gunshot_f, 'hf': human_f, 'lf' : logging_f, 
-           'lo' : logging_o, 'po' : poaching_o}
+    template_loader = jinja2.FileSystemLoader('./')
+    template_env = jinja2.Environment(loader=template_loader)
 
-template_loader = jinja2.FileSystemLoader('./')
-template_env = jinja2.Environment(loader=template_loader)
+    file_template = r'pdf-template.html'
+    template = template_env.get_template(file_template)
+    output_text = template.render(context)
 
-file_template = r'pdf-template.html'
-template = template_env.get_template(file_template)
-output_text = template.render(context)
+    config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
+    pdfkit.from_string(output_text, 'generated_data.pdf',
+                        configuration=config, css = 'style.css')
 
-config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
-pdfkit.from_string(output_text, 'generated_data.pdf', configuration=config, css = 'style.css')
+generatePdf(15, 15, 6, 92, 58, 59, 83)
